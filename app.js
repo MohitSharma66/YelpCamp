f8f3b5f820fe7ff2799d4f68ae092a88f38d17ed
Mohit Sharma <mohits9168@gmail.com>
2024-08-25 22:30:11 +0530

    Final

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public'))); //the scripts directly go in the public folder
 
-const store = new MongoStore.create({
+const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
